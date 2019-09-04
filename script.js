0d403ad63a0b662958c064a89bab1fc9069bffdb
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-09-03 22:22:34 -0400

    slight modifications

--- script.js
+++ script.js
@@ -16,19 +16,23 @@ const doResize = () => {
 window.onresize = doResize;
 window.onload = doResize;
 
+window.onscroll = function () {
+
+};
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const shellInputType = async text => {
     let i = 0;
     byId("shell-input").innerText = "";
     while (i < text.length) {
-        if (byId("shell-input").innerText.length !== i) {
+        if (byId("shell-input").innerText !== text.substring(0, i)) {
             break;
         }
 
         byId("shell-input").innerText += text.charAt(i);
         i++;
-        await sleep(35);
+        await sleep(50);
     }
 };
 
@@ -36,6 +40,20 @@ const shellInputBlank = () => {
     byId("shell-input").innerText = "";
 };
 
-byId("anchor-about").onmouseover = () => shellInputType("cd About/");
-
+byId("anchor-about").onmouseover = () => shellInputType("cd about/");
 byId("anchor-about").onmouseleave = shellInputBlank;
+
+byId("anchor-contact").onmouseover = () => shellInputType("ping jonathan");
+byId("anchor-contact").onmouseleave = shellInputBlank;
+
+byId("anchor-experience").onmouseover = () => shellInputType("history");
+byId("anchor-experience").onmouseleave = shellInputBlank;
+
+byId("anchor-projects").onmouseover = () => shellInputType("git clone projects");
+byId("anchor-projects").onmouseleave = shellInputBlank;
+
+byId("anchor-resume").onmouseover = () => shellInputType("curl resume.pdf");
+byId("anchor-resume").onmouseleave = shellInputBlank;
+
+byId("anchor-skills").onmouseover = () => shellInputType("cat skills");
+byId("anchor-skills").onmouseleave = shellInputBlank;
